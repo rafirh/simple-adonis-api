50bf94acf75975bfbc40d4762b08a9c87c304bdc
Rafi Rahman <rafirahmann18@gmail.com>
2022-06-05 08:30:32 +0700

    fisrt commit

--- test.js
+++ test.js
@@ -1,12 +1,13 @@
-function square(a, b){
-    let result = 1;
-    for (let i = 0; i < b; i++) {
-        result *= a
+let sum = (x, y, callBack) => {
+    result = x + y
+    if (typeof callBack == 'function'){
+        result = callBack(x, y)
     }
     return result
 }
-function print(c, d, callBack){
-    console.log(callBack(c, d))
-}
 
-print(2,99,square)
+a = sum(2, 2, (x, y) =>{
+    return "The result is " + (x + y)
+})
+
+console.log(a);
